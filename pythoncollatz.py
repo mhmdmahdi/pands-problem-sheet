516fb15ae6b47ed37f97c48a54cae57a22fb5e5f
mohammed mahdi <mohammed@mohammed-mahdis-MacBook-Pro.local>
2022-02-21 23:37:10 +0700

    test

--- pythoncollatz.py
+++ pythoncollatz.py
@@ -1,6 +1,7 @@
 integer = int(input("Please input a positive integer:"))
 while (integer <= 0):
     integer = int(input("Please input a positive integer:"))
+# this while loop ensures that only a postive integer which is not 0 can be inputted. Any negative values or 0 will not be accepted.
 
 x = integer
 
@@ -11,3 +12,5 @@ while x > 1:
     elif (x % 2 == 1):
         x = ((x * 3) + 1)
     print(x)
+# if/ elif statement within while loop to continue iterations to completion. Code completes once final result of 2 is reached as this is greater than 1 and once divided by 2 gives a result of 1
+# modulus is used to determine if the value inputted is odd or even as described in the Week 4 lectures
