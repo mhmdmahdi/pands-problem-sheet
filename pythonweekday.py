98b8b5d95687cf8fc49c48cca0f6751b5d7c8cc1
mohammed mahdi <mohammed@mohammed-mahdis-MacBook-Pro.local>
2022-02-25 18:31:34 +0700

    test

--- pythonweekday.py
+++ pythonweekday.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-# importing method
+# importing method to get the day of the week
 # reference: https://www.delftstack.com/howto/python/python-datetime-day-of-week/#:~:text=of%20the%20day.-,Use%20the%20weekday()%20Method%20to%20Get%20the%20Name%20of,0%20and%20Sunday%20is%206.
 
 days = ["Monday", "Tuesday", "Wednesday",
