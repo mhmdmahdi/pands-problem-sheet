14bce9279ebd49b28fadd3f1cfef37ba0e5ea2a4
mohammed mahdi <mohammed@mohammed-mahdis-MacBook-Pro.local>
2022-04-08 22:32:40 +0700

    test

--- pythoncollatz.py
+++ pythoncollatz.py
@@ -7,8 +7,9 @@ x = integer
 
 while x > 1:
     if (x % 2 == 0):
-        x = (round(x // 2))
+        x = (round(x / 2))
 # reference to round integer to whole numbers https://kodify.net/python/math/round-integers/
+# // returns an integer and / returns a float as per Andrews feedback
     elif (x % 2 == 1):
         x = ((x * 3) + 1)
     print(x)
